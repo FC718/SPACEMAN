c24c613cd33f72f9bd1e6011e51b1e5119f65901
FC718 <FrankCasseus@gmail.com>
2022-10-11 19:30:16 -0400

    new commit

--- js/main.js
+++ js/main.js
@@ -9,34 +9,53 @@ let wrongLetters; // Object key of 'P' -> For the players wrong answers
 let winner; // String 'p" if player wins. 'l' for lose.
 let guessWord; 
 
+
+// selecting HTML
 // --------STORED/CACHED ELEMENTS-------
+
+// getting all of the section elements and the children VVVV
   const letterEls = document.querySelectorAll('#letters > button') 
   console.log(letterEls)
 
   const pGuessWord = document.getElementById('p-guessword')
   console.log(pGuessWord)
 
+  const homeButton = document.getElementsByClassName('homeBtn')
+  
 
+
+
+
+  // attatchig query selector to the section
 // --- EVENT LISTENERS ----
 // This is a click event handler for when the user clicks on the letter buttons.
 document.querySelector('#letters')
 .addEventListener('click', handleChoice)
+// document.getElementsByClassName('#homebtn', handleNewWord)
+// homeButton.addEventListener('click', takeMeHome)
+
+
+
 
 
 // Coding our Render Functions
 // -------Functions---------
-init()
+init();  
 // Initialize all state, then call render();
 function init() {
+  // array of the winning words
   secretWord = winningWords[Math.floor(Math.random() * winningWords.length )]
-  console.log(secretWord)
+  console.log(secretWord) 
   
-  wrongLetters = []
+  wrongLetters = [];
   
+
   //  Created a guess word. For dashes to be shown on the DOM
   guessWord = '';
   
-  for(let letter of secretWord ) {
+
+  // This shows the underscores and if theres no word there itll leave it empty
+  for(let letter of secretWord) {
     guessWord = guessWord + (letter === ' ' ? ' ' : '_')
   }
   
@@ -48,9 +67,7 @@ function init() {
     const letter = evt.target.id
     console.log(evt.target.id)
     if (secretWord.toLowerCase().includes(letter)) {
-      let newGuessWord = '' 
-      
-
+      let newGuessWord = ''; 
       for (let i = 0; i < secretWord.length ; i++) {
         if (secretWord.toLowerCase().charAt(i) === letter) {
           
@@ -58,21 +75,35 @@ function init() {
         } 
         else {
           newGuessWord = newGuessWord + guessWord.charAt(i)
-        }
-        
+        } 
       }
       guessWord = newGuessWord
       console.log(newGuessWord)
-
     }
-    render()
+    render();
 
   }  
+      //  Also trying to figure out how to show how many remaining letters / chances you get
+      // TRYING TO FIGURE OUT HOW TO RENDER THIS MESSAGE WHEN YOU CHOOSE THE CORRECT BTN
+      function renderMessage() {
+
+        if(gameStatus === 'W') {
+          messageChannel.textContent = 'Congrats!';
+          
+        } else if (gameStatus === 'L') {
+          messageChannel.innerText = `Youre out in space! The answer was ${randomWord.join()}` 
+  
+        } else 
+          livesEl.textcontent = `${maxWrong - wrongGuesses.length} lives remain good luck)`
+      }
+      
+
+      
 
 
 function render() {
   // renderSecretWord();
-  console.log('hello');
+  // console.log('hello');
   // renderwrongAnswer();
   pGuessWord.textContent = guessWord
   
